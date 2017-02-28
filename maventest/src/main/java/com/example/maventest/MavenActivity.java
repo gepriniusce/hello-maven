61f16570282b80813b6a704fbb2da0c83a751a80
kongming <luzhanneng@linghit.com>
2017-02-28 17:02:35 +0800

    103

--- maventest/src/main/java/com/example/maventest/MavenActivity.java
+++ maventest/src/main/java/com/example/maventest/MavenActivity.java
@@ -1,13 +1,23 @@
 package com.example.maventest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MavenActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maven);
+        TextView textView = (TextView) findViewById(R.id.text);
+        textView.setText("23333333333333333333333");
     }
+
+
+    public void setText(String s) {
+        textView.setText("s:" + s);
+    }
+
 }
